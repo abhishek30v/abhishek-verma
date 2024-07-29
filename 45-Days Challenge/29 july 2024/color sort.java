fccdbd08649fd15e36361d2d34b9731f9d582368
Abhishek Verma <av7257383@gmail.com>
2024-07-29 23:09:05 +0530

    done

---
+++ 45-Days Challenge/29 july 2024/color sort.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            int curr = nums[i];
+            int prev= i-1;
+            while(prev>=0 && nums[prev] > curr){
+                nums[prev+1] = nums[prev];
+                prev--;
+            }
+            nums[prev+1] = curr;
+        }
+    }
+}
