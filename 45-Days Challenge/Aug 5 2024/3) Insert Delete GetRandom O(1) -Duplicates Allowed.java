224373a053aba5cabd4521fa1cdeb8d28f505a44
Abhishek Verma <av7257383@gmail.com>
2024-08-06 14:22:17 +0530

    6th day ques

--- 45-Days Challenge/Aug 5 2024/3) Insert Delete GetRandom O(1) -Duplicates Allowed.java
+++ 45-Days Challenge/Aug 5 2024/3) Insert Delete GetRandom O(1) -Duplicates Allowed.java
@@ -1,40 +1,73 @@
 class RandomizedCollection {
-    HashMap<Integer,Integer> hm;
+    HashMap<Integer,HashSet<Integer>> map;
     List<Integer> list;
+    Random random;
         public RandomizedCollection() {
-            hm = new HashMap<>();
+            map = new HashMap<>();
             list = new ArrayList<>();
+            random = new Random();
         }
         
         public boolean insert(int val) {
-            if(hm.containsKey(val)){
-                return false;
+            boolean flag;
+            HashSet<Integer> set;
+            if(map.containsKey(val)){
+                set = map.get(val);
+                flag = false;
             }
-            list.add(val);
-            int idx = list.size()-1;
-            hm.put(val,idx);
-            return true;
+           else{
+               set = new HashSet<>();
+               flag = true;
+           }
+           list.add(val);
+           set.add(list.size()-1);
+           map.put(val,set);
+           return flag;
         }
         
         public boolean remove(int val) {
-            if(!hm.containsKey(val)){
-                return false;
-            }
-            int idx = hm.get(val);
-            Collections.swap(list,idx,list.size()-1);
-            int swappedval = list.get(idx);
+           if(!map.containsKey(val)){
+            return false;
+           }
+           HashSet<Integer>set;
+           set = map.get(val);
+           int idx=-1;
+           for(int itr : set){
+               idx = itr;
+               break;
+           }
     
-            if(idx < list.size()){
-                hm.put(swappedval,idx);
-            }
-            list.remove(list.size()-1);
-            hm.remove(val);
-            return true;
+           set.remove(idx);
+           if(set.isEmpty()){
+            map.remove(val);
+           }else
+           map.put(val,set);
+           
+           if(idx == list.size()-1){
+               list.remove(list.size()-1);
+           }else{
+                Collections.swap(list,idx,list.size()-1);
+                list.remove(list.size()-1);
+                int newval = list.get(idx);
+                HashSet<Integer> temp;
+                temp = map.get(newval);
+                temp.add(idx);
+                temp.remove(list.size());
+                map.put(newval,temp);
+           }
+           return true;
         }
         
         public int getRandom() {
-            Random random = new Random();
-            int n = random.nextInt(list.size());
-            return list.get(n);
+            int idx = random.nextInt(list.size());
+            return list.get(idx);
         }
     }
+    
+    /**
+     * Your RandomizedCollection object will be instantiated and called as such:
+     * RandomizedCollection obj = new RandomizedCollection();
+     * boolean param_1 = obj.insert(val);
+     * boolean param_2 = obj.remove(val);
+     * int param_3 = obj.getRandom();
+     */
