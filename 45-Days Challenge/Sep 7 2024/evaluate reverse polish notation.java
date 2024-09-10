3b39499a0fc24bc34cbd0912373e35ffc31d42ba
Abhishek Verma <av7257383@gmail.com>
2024-09-11 04:28:16 +0530

    39th day

--- 45-Days Challenge/Sep 7 2024/evaluate reverse polish notation.java
+++ 45-Days Challenge/Sep 7 2024/evaluate reverse polish notation.java
@@ -1,5 +1,29 @@
-package abhishek-verma.45-Days Challenge.Sep 7 2024;
+class Solution {
+    public int operate(int a,int b,String token){
+        switch(token){
+            case "+" : return a+b;
+            case "-" : return a-b;
+            case "*" : return a * b;
+            case "/" : return a/b;
+            default : throw new IllegalArgumentException("Invalid");
+        }
+    }   
 
-public class evaluate reverse polish notation {
-    
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int result =0;
+        for(String token : tokens){
+            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
+                int b = st.peek();
+                st.pop();
+                int a = st.peek();
+                st.pop();
+                result = operate(a,b,token);
+                st.push(result);
+            }else{
+                st.push(Integer.parseInt(token));
+            }
+        }   
+ return st.peek();
+   }
 }
